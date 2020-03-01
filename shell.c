cba7b0eae30c1453bc53475f9c05575ccbbb4132
Spencer Axford <saxford@mun.ca>
2020-03-01 01:25:07 -0330

    Added interrupt signal handler
    General code cleanup

--- shell.c
+++ shell.c
@@ -1,3 +1,8 @@
+/*
+    Triton Shell
+    Created by: Spencer Axford
+*/
+
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -7,27 +12,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h> 
 
-char* prompt_user(char * buffer, size_t bufsize) {
-    write(1,"triton> ", 8);
-    size_t chars = getline(&buffer, &bufsize, stdin);
-    if ((buffer)[chars - 1] == '\n') {
-        (buffer)[chars - 1] = '\0';
-    }
-    return buffer;
-}
+void    sig_int_handler(); // handles ctrl - c SIGINT terminations
+char*   prompt_user(char * buffer, size_t bufsize); // prompts user for command
 
-int main(int argc, char *argv[])
-{
+int children; // number of child processes
+int parent_pid; // top parent pid number
+
+int main(int argc, char *argv[]) {
+    // interrupt handler setup
+    signal(SIGINT,sig_int_handler);
     const int max_num_args = 50;
     const char delim = ' ';
-    while(1){
+    children = 0;
+    parent_pid = getpid();
+    while(true) {
         char *buf;
         char* c_buf = buf;
         size_t bufsize = 128;
+        
         buf = (char *)calloc(bufsize,sizeof(char));
-        if( buf == NULL)
-        {
+        if( buf == NULL) {
             err(-1, "Unable to allocate buffer");
         }
 
@@ -40,11 +46,11 @@ int main(int argc, char *argv[])
                 err(-1, "Error in fork()");
 
             case 0: {
-                char* c_buf = buf;
+                children = 0;
                 char* args[max_num_args];
                 int n_args = 0;
-                while (c_buf != NULL && n_args < max_num_args-1){
-                    char* arg = strsep(&c_buf, &delim);
+                while (buf != NULL && n_args < max_num_args-1){
+                    char* arg = strsep(&buf, &delim);
                     args[n_args] = arg;
                     n_args = n_args + 1;
                 }
@@ -56,11 +62,17 @@ int main(int argc, char *argv[])
 
             default: {
                 int status;
+                children += 1;
                 if (waitpid(child, &status, 0) == -1) {
                     err(-1, "Failed to waitpid()");
                 }
                 if (WIFEXITED(status)) {
                     printf("exited with code: %d\n", WEXITSTATUS(status));
+                    children -= 1;
+                }
+                if (WIFSIGNALED(status)) {
+                    printf("\nprocess terminated\n");
+                    children -= 1;
                 }
                 free(buf);
                 break;
@@ -68,4 +80,25 @@ int main(int argc, char *argv[])
         }
     }
     return 0;
+}
+
+void sig_int_handler() {
+    // If the process is a child process, terminate
+    if (getpid() != parent_pid) {
+        exit(128 + SIGINT);
+    } else {
+        // If this is the parent process, and there are no remaining children, terminate
+        if (children == 0) {
+            exit(128 + SIGINT);
+        }
+    }
+}
+
+char* prompt_user(char * buffer, size_t bufsize) {
+    write(1,"triton> ", 8);
+    size_t chars = getline(&buffer, &bufsize, stdin);
+    if ((buffer)[chars - 1] == '\n') {
+        (buffer)[chars - 1] = '\0';
+    }
+    return buffer;
 }
